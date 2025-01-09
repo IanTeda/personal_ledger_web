646738070fde502a2576e3932d565309d2aebccb
Ian Teda <ian@teda.id.au>
2025-01-09 21:12:10 +1100

    Add schadcn_ui

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button"
 
 export const Route = createFileRoute('/login')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/login"!</div>
+  return (
+    <>
+      <div>Hello "/login"!</div>
+      <Button>Click me</Button>
+    </>
+
+  )
 }
