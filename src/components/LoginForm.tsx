306df6d2b8024c64c3e75389d22f30cefda3106e
Ian Teda <ian@teda.id.au>
2025-02-24 08:08:20 +1100

    Working on query authentication

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,9 +4,15 @@
 //
 // Login form for user authentication
 //
+// ## For Testing
+//
+// email: default_ams@teda.id.au
+// password: S3cret-Admin-Pas$word!
+//
 // ## References
 //
 // 1. [Nv4n/idea-board-spring-boot](https://github.com/Nv4n/idea-board-spring-boot/blob/master/src/react/src/components/board/CreateNoteForm.tsx)
+// 2. [](https://github.com/ysmello/react-tanstack-auth/blob/main/src/state/queries/sign-in.ts)
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -20,19 +26,89 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useForm } from "@tanstack/react-form";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { LoginRequest, TokenResponse } from "@/lib/grpc/authentication/authentication";
+import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
+import { AuthenticationServiceClient } from "@/lib/grpc/authentication/authentication.client";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
-  const form = useForm({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: async ({ value }) => {
-      // Do something with form data
+
+  const queryClient = useQueryClient();
+
+  const { data } = useQuery({
+    queryKey: ["tokens"],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      
+      const response = TokenResponse.create({
+        accessToken: "access_token",
+        refreshToken: "refresh_token",
+      });
+
+      return response;
+    }
+  });
+
+  const updateTokens = useMutation({
+    mutationKey: ["tokens"],
+    mutationFn: async (value: { email: string; password: string }) => {
+      console.log("Mutate tokens");
       console.log(value);
+
+      // Create a new transport layer
+      const transport = new GrpcWebFetchTransport({
+        baseUrl: "http://localhost:8091",
+      });
+
+      // Create a new Authentication service client
+      const client = new AuthenticationServiceClient(transport);
+
+      // Building login request object
+      const request = LoginRequest.create({
+        email: value.email,
+        password: value.password,
+      });
+
+      // Send login request to authentication client
+      const { response } = await client.login(request);
+
+      console.log("Access Token: ", response.accessToken);
+      console.log("Refresh Token: ", response.refreshToken);
+
+      return response;
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["tokens"], data);
+    }
+
+  });
+
+  const form = useForm({
+    // Set default form values
+    defaultValues: {
+      email: "default_ams@teda.id.au",
+      // cSpell:ignore S3cret
+      password: "S3cret-Admin-Pas$word!",
+    },
+
+    // Form submission handler. Do something with form data
+    onSubmit: async ({ value }) => {
+      console.log("Login form submit:", value);
+      // Do something with form data
+      await updateTokens.mutateAsync(value);
+
+      // const { accessToken, refreshToken } = await sendLoginRequest(
+      //   value.email,
+      //   value.password
+      // );
+
+      // console.log("Access Token: ", accessToken);
+      // console.log("Refresh Token: ", refreshToken);
+
+      // await saveTokens.mutateAsync(value);
     },
   });
 
@@ -106,6 +182,9 @@ export function LoginForm({
           </form>
         </CardContent>
       </Card>
+      <div>
+        <pre>{data?.accessToken}</pre>
+      </div>
     </div>
   );
 }
